4b088ee9077d0e706c2f222612039e8e94f2b873
roytheboyyy <roy.mou.safa@gmail.com>
2025-08-31 17:01:19 +0300

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,5 @@
+# I accidentally made this forgetting my topic. Hopefully this is good!.
+i = int(input("Enter number, And i will calculate the Ammount of digits in it."))
+for n in range(i):
+    print(i-n)
+print("0")
